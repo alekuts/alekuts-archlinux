72629ca7553f4f068cf556828813c5a7e8fee1e9
AleKutS <alexanderkutsovol@gmail.com>
2024-08-02 19:45:52 +0300

    Add files via upload

--- alekuts-archlinux.sh
+++ alekuts-archlinux.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 
-# Section 1
+
+# Disk
 fdisk -l
 
 read $disk
@@ -27,15 +28,20 @@ mkdir /mnt/home
 mount /dev/$disk$diskPart /mnt/home
 
 
-# Section 2
+
+# Kernel and system
 while ! pacstrap -K --noconfirm /mnt base linux linux-firmware ; do : ; done
 
 genfstab /mnt -U >> /mnt/etc/fstab
 
 
-# Section 3
+
+# System root
 root="arch-chroot /mnt"
 
+
+
+# Programs
 $root pacman-key --init
 
 $root pacman-key --populate
@@ -47,13 +53,17 @@ $root sed -i '/\[multilib\]/ {n;s/#//}' /etc/pacman.conf
 $root while ! pacman -Syu --noconfirm dhcpcd sudo grub efibootmgr xorg xorg-xinit noto-fonts noto-fonts-emoji noto-fonts-cjk font-manager pulseaudio pulseaudio-alsa alsa-utils fuse2 libmtp gvfs-mtp thunar flameshot git steam wine wine-mono wine-gecko nvidia ; do : ; done
 
 
-# Section 4
+
+# Time
 $root systemctl enable dhcpcd
 
 $root ln -sf /usr/share/zoneinfo/Europe/Kyiv /etc/localtime
 
 $root hwclock --systohc
 
+
+
+# Locale
 $root sed -Ei 's/#(de_DE.*UTF-8)/\1/' /etc/locale.gen
 
 $root sed -Ei 's/#(en_US.*UTF-8)/\1/' /etc/locale.gen
@@ -64,6 +74,9 @@ $root sed -Ei 's/#(uk_UA.*UTF-8)/\1/' /etc/locale.gen
 
 $root locale-gen
 
+
+
+# Computer and user name
 $root echo "archlinux" > /etc/hostname
 
 $root echo "127.0.0.1 localhost
@@ -83,7 +96,8 @@ $root EDITOR=nvim
 $root sed -Ei 's/# (%wheel ALL.*ALL\) ALL)/\1/' /etc/sudoers
 
 
-# Section 5
+
+# GRUB
 $root mkdir /boot/EFI
 
 $root diskPart=1
@@ -94,6 +108,14 @@ $root grub-install --target=x86_64-efi --efi-directory=/boot/EFI
 
 $root grub-mkconfig -o /boot/grub/grub.cfg
 
+
+
+# Xinit config
+$root echo "xrandr --output DP-0 --mode 1920x1080 --rate 165" > .xinitrc
+
+
+
+# Unmount and reboot
 umount /mnt -l
 
 reboot
