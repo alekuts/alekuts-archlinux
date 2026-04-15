5709b7b8db6515d7f226159d15ed9a28ee32e804
AleKutS <alexanderkutsovol@gmail.com>
2026-04-15 22:37:58 +0300

    upd

--- postaalinux.sh
+++ postaalinux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-while ! sudo pacman -Syu --noconfirm base-devel xorg xorg-xinit noto-fonts noto-fonts-emoji noto-fonts-cjk pipewire pipewire-pulse pipewire-alsa alsa-utils easyeffects lsp-plugins fuse2 libmtp gvfs-mtp thunar thunar-archive-plugin xarchiver 7zip kitty flameshot git steam wine zed fastfetch scrcpy kdenlive pavucontrol qpwgrap obs-studio telegram-desktop reaper duf compsize ntfs3g btop awesome rofi gammastep lxappearance rofi nvidia nvidia-utils ; do : ; done
+while ! sudo pacman -Syu --noconfirm base-devel xorg xorg-xinit noto-fonts noto-fonts-emoji noto-fonts-cjk pipewire pipewire-pulse pipewire-alsa alsa-utils easyeffects lsp-plugins fuse2 libmtp gvfs-mtp thunar thunar-archive-plugin xarchiver 7zip kitty flameshot git steam wine zed fastfetch scrcpy kdenlive pavucontrol qpwgraph obs-studio telegram-desktop reaper duf compsize ntfs-3g btop awesome rofi gammastep lxappearance rofi nvidia-utils ; do : ; done
 
 cd
 while ! git clone https://aur.archlinux.org/yay.git ; do sleep 1 ; done
@@ -9,7 +9,9 @@ makepkg -si
 cd
 yay -S zen-browser-bin vesktop-bin
 
-mv alekuts-archlinux/.config /home/alekuts/
+git clone https://github.com/alekuts/alekuts-archlinux
+
+cp -rf alekuts-archlinux/.config/. /home/alekuts/.config/
 mv alekuts-archlinux/.themes /home/alekuts/
 mv alekuts-archlinux/.icons /home/alekuts/
 mv alekuts-archlinux/.xinitrc /home/alekuts/
