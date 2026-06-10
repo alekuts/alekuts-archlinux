d74313fc546f590702899abf4cd75949defb69d9
AleKutS <alexanderkutsovol@gmail.com>
2026-06-11 00:18:44 +0300

    config update

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -212,7 +212,7 @@ screen.connect_signal("request::desktop_decoration", function(s)
             {             -- Right widgets
                 layout = wibox.layout.fixed.horizontal,
                 mykeyboardlayout,
-                wibox.widget.systray(),
+		wibox.widget.systray(),
                 mytextclock,
                 s.mylayoutbox,
             },
